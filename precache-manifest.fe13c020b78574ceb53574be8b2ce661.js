eae2640f74a8f4b086088d8d8eb8f677fa13894d
Viktor Nareiko <vnareiko.lt@gmail.com>
2019-11-21 09:49:08 +0200

    Updates

---
+++ precache-manifest.fe13c020b78574ceb53574be8b2ce661.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8174102f222667a4fc1aedd6f57eef07",
+    "url": "/Adapt-Academy-Frontend-2019-task/index.html"
+  },
+  {
+    "revision": "ccefbb8c19a04852323d",
+    "url": "/Adapt-Academy-Frontend-2019-task/static/css/main.80645a2f.chunk.css"
+  },
+  {
+    "revision": "47bc710b35b1af16fe10",
+    "url": "/Adapt-Academy-Frontend-2019-task/static/js/2.b95e9abd.chunk.js"
+  },
+  {
+    "revision": "ccefbb8c19a04852323d",
+    "url": "/Adapt-Academy-Frontend-2019-task/static/js/main.5fc650aa.chunk.js"
+  },
+  {
+    "revision": "a49790e0b6f7338c8b1d",
+    "url": "/Adapt-Academy-Frontend-2019-task/static/js/runtime-main.605e6a30.js"
+  }
+]);
